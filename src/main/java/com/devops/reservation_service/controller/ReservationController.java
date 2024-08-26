be3b76eae5b1e8b3ffc4a097b404ce9230f98e95
Marko Dragicevic <marko.dragicevic@levi9.com>
2024-08-26 19:05:24 +0200

    feat: host accepts or denies reservation request

--- src/main/java/com/devops/reservation_service/controller/ReservationController.java
+++ src/main/java/com/devops/reservation_service/controller/ReservationController.java
@@ -3,12 +3,12 @@ package com.devops.reservation_service.controller;
 import com.devops.reservation_service.dto.ReservationDto;
 import com.devops.reservation_service.model.Reservation;
 import com.devops.reservation_service.service.ReservationService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/reservations")
@@ -23,5 +23,17 @@ public class ReservationController {
     @PostMapping
     public Reservation createReservation(Principal principal, @RequestBody ReservationDto reservationDto) {
         return reservationService.createReservationRequest(principal.getName(), reservationDto);
+    }
+
+
+    @PutMapping("/hosts/respond/{reservationId}")
+//  todo: @HasRole("HOST")
+    public ResponseEntity<?> respondToReservationRequest(
+            Principal principal,
+            @PathVariable UUID reservationId,
+            @RequestParam boolean accepted
+    ) {
+        reservationService.respondToReservationRequest(principal.getName(), reservationId, accepted);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
